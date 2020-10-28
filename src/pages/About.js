1757395a262725950abe185b56cad8524f4541e2
smpawar001 <smpawar001@gmail.com>
2020-10-28 11:42:28 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+        distinctio ut culpa enim, ipsam illum earum soluta reiciendis quod,
+        iusto quas rem error. Earum aliquid quibusdam itaque similique esse ea
+        cumque ipsa sequi officiis, distinctio soluta dolore reiciendis ut
+        quasi?
+      </p>
+    </section>
+  )
+}
+
+export default About
